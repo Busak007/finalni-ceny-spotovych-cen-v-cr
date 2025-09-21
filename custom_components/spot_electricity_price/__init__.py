edfaab7482e32c1d2884825ce3423280f4d38b84
Radek Buš <94534956+Busak007@users.noreply.github.com>
2025-09-21 07:54:01 +0200

    Update __init__.py
    Úprava pro funkčnost s novou verzí HA

--- custom_components/spot_electricity_price/__init__.py
+++ custom_components/spot_electricity_price/__init__.py
@@ -12,17 +12,15 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Spot Electricity Price from a config entry."""
     # Forward the entry setup to both sensor and binary_sensor platforms
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "binary_sensor"])
+    
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     # Unload both sensor and binary_sensor platforms
-    unload_sensor = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    unload_binary_sensor = await hass.config_entries.async_forward_entry_unload(entry, "binary_sensor")
-    return unload_sensor and unload_binary_sensor
+    unload_ok = await hass.config_entries.async_forward_entry_unload_platforms(
+        entry, ["sensor", "binary_sensor"]
+    )
+    
+    return unload_ok
